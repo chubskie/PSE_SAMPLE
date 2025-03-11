827c122d26b19f23b40d5990a55f025a8e508699
Chubskie <chazzmanubay@gmail.com>
2025-03-12 03:15:27 +0800

    Used prepared statements.

--- delete.php
+++ delete.php
@@ -5,12 +5,23 @@ include "functions.php";
 
 if (isset($_POST['delete'])) {
     $id = sanitizeInput($_POST['id']);
-    $query = "DELETE FROM residents WHERE resident_code = '$id';";
 
-    if ($connection->query($query)) {
-        $connection->close();
-        header("Location: index.php");
+    $statement = $connection->prepare("DELETE FROM residents WHERE resident_code = ?");
+
+    if ($statement) {
+        $statement->bind_param("s", $id);
+
+        if ($statement->execute()) {
+            $statement->close();
+            header("Location: index.php");
+            exit;
+        } else {
+            error_log("Delete error: " . $statement->error);
+            echo "Error: Failed to delete resident.";
+            $statement->close();
+        }
     } else {
-        echo "Error: " . $connection->error;
+        error_log("Prepare error: " . $connection->error);
+        error_log("Database error: " . $connection->error);
     }
 }
